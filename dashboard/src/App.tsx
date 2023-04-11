259d585f8c1fb023f40ebfe99ed04fe339be6de2
Pedro Miguel <pedromrap@gmail.com>
2023-04-11 11:04:59 -0300

    config: setup tailwind css

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,5 +1,9 @@
+import './styles/global.css';
+
 function App() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="text-4xl font-bold underline text-white">Dashboard</div>
+  );
 }
 
 export default App;
